489f35b88cb117a0a271cdcda15f81a6e0f0e707
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-02 17:38:46 +0530

    Update outputs.tf

--- devops-infra/outputs.tf
+++ devops-infra/outputs.tf
@@ -1,1 +1,9 @@
+# Cluster Name Output
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
 
+# Cluster Endpoint (API URL)
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
