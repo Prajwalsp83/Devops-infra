438124232174bd2b23c558c1a2776618cf794390
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-02 17:37:55 +0530

    Update variables.tf

--- devops-infra/variables.tf
+++ devops-infra/variables.tf
@@ -1,1 +1,11 @@
+# AWS Region
+variable "region" {
+  description = "AWS region"
+  default     = "ap-south-1"   # 🔴 UPDATE HERE if needed
+}
 
+# EKS Cluster Name
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  default     = "devops-cluster"   # 🔴 UPDATE HERE (any name you like)
+}
